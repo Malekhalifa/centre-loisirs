f52502f634efdddafab595f760dd83fb526169b2
Malek khalifa <78558399+Malekhalifa@users.noreply.github.com>
2025-02-16 16:02:51 +0100

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDAbXb-4KchKF3LOVHQGtvZ_1w_KZdEoqo",
-  authDomain: "centre-loisirs-2d24b.firebaseapp.com",
-  projectId: "centre-loisirs-2d24b",
-  storageBucket: "centre-loisirs-2d24b.firebasestorage.app",
-  messagingSenderId: "440590528711",
-  appId: "1:440590528711:web:28e1da3208721469c18ee2"
+  apiKey: "#####################################",
+  authDomain: "#####################################",
+  projectId: "#####################################",
+  storageBucket: "#####################################",
+  messagingSenderId: "############################",
+  appId: "#####################################"
 };
 
 // Initialize Firebase
